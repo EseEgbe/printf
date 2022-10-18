329835558e05142b5dfe875191ad0bc20046faff
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-10-18 14:52:49 -0700

    main header file

--- main.h
+++ main.h
@@ -19,5 +19,13 @@ typedef struct printHandler
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int get_flag(char s, flags_t *f);
+int (*get_print(char s))(va_list, flags_t *);
+int print_int(va_list l, flags_t *f);
+int count_digit(int i);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+char *convert(unsigned long int num, int base, int lowercase);
+int _puts(char *str);
 
 #endif
